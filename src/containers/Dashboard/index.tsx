0dfb425a9d260675f7475e1d20986d3501657ea6
kamal11-coder <gopidikamal@gmail.com>
2020-08-18 15:33:19 +0530

    disigned home page and added new components

---
+++ src/containers/Dashboard/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const dashboard: React.CSSProperties = {
+  textAlign:"center",
+  paddingTop:"100px"
+};
+
+const Dashboard: React.FC = () => {
+  return <div style={dashboard}>Dashboard</div>;
+};
+
+export default Dashboard;
---
+++ src/containers/Edit/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const edit: React.CSSProperties = {
+  textAlign:"center",
+  paddingTop:"100px"
+};
+
+const Edit: React.FC = () => {
+  return <div style={edit}>Edit</div>;
+};
+
+export default Edit;

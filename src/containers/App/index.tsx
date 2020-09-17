e59d67a9d860739da3fd00841faee31351efea85
Jeffrey Asmus <jeffreyasmus@Jeffreys-MacBook-Pro.local>
2020-09-17 16:41:03 -0700

    Add me get request

--- src/containers/App/index.tsx
+++ src/containers/App/index.tsx
@@ -1,15 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+import { API_URI } from "../../constants";
 
 interface Props {
   signOut: () => void;
 }
 
 const App: React.FC<Props> = ({ signOut }) => {
+  const [user, setUser] = useState(null);
   return (
     <div>
       Timesheets
       <p>Hey! Let's clock in.</p>
       <button onClick={signOut}>Log out</button>
+      <button
+        onClick={async () => {
+          const data = await axios.get(`${API_URI}/auth/me`, {
+            withCredentials: true
+          });
+          console.log(data);
+        }}
+      >
+        Get me
+      </button>
+      <pre>{user}</pre>
     </div>
   );
 };
